fab66e88523bf81248d7844d7b906f701cc23c53
Bryan Miramira <bbqms@hotmail.com>
2020-07-06 17:54:20 -0500

    corpus

---
+++ Corpus.py
@@ -0,0 +1,29 @@
+import nltk
+from nltk.corpus import PlaintextCorpusReader
+from nltk.text import Text
+
+ruta = 'datasets'
+archivo = 'positive_tweets.txt'
+lectordetexto = PlaintextCorpusReader(ruta,archivo,encoding='utf8')
+temp = lectordetexto.words()
+texto = Text(temp)
+print(temp)
+
+"""
+print(texto.count('inteligencia'))
+print(texto.index('inteligencia'))
+print(texto.concordance('inteligencia'))
+print("dispersion")
+texto.dispersion_plot(['inteligencia','maquina'])
+print("frecuencia")
+Frecuencia = {'inteligencia':20,'maquina':11,'aprendizaje':5,'malo':1}
+print(Frecuencia['inteligencia'])
+print(Frecuencia.keys())
+print(Frecuencia.values())
+print(Frecuencia.items())
+print(str(Frecuencia))
+texto1 = ['hola','quetal','hola']
+fdist = nltk.FreqDist(texto1)
+print(fdist)
+print(list(nltk.FreqDist.keys(fdist)))
+print(list(nltk.FreqDist.values(fdist)))"""
